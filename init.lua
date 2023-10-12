4f9d2535a99acee41b62cc3bd7dc4e7834c3e392
Szuruburu <szuru.buru@hotmail.com>
2023-10-12 11:29:01 +0200

    mainly plugins

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ require "core"
 local custom_init_path = vim.api.nvim_get_runtime_file("lua/custom/init.lua", true)[1]
 
 if custom_init_path then
-  dofile(custom_init_path)
+	dofile(custom_init_path)
 end
 
 require("core.utils").load_mappings()
@@ -12,10 +12,75 @@ local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
 
 -- bootstrap lazy.nvim!
 if not vim.loop.fs_stat(lazypath) then
-  require("core.bootstrap").gen_chadrc_template()
-  require("core.bootstrap").lazy(lazypath)
+	require("core.bootstrap").gen_chadrc_template()
+	require("core.bootstrap").lazy(lazypath)
 end
 
 dofile(vim.g.base46_cache .. "defaults")
 vim.opt.rtp:prepend(lazypath)
 require "plugins"
+
+require("toggleterm").setup{
+	-- size can be a number or function which is passed the current terminal
+	size = function(term)
+		if term.direction == "horizontal" then
+			return 15
+		elseif term.direction == "vertical" then
+			return vim.o.columns * 0.4
+		end
+	end,
+	open_mapping = [[<c-\>]],
+	hide_numbers = true, -- hide the number column in toggleterm buffers
+	shade_filetypes = {},
+	shade_terminals = true,
+	shading_factor = 3, -- the degree by which to darken to terminal colour, default: 1 for dark backgrounds, 3 for light
+	start_in_insert = true,
+	insert_mappings = true, -- whether or not the open mapping applies in insert mode
+	persist_size = true,
+	direction = 'horizontal',
+	close_on_exit = true, -- close the terminal window when the process exits
+	shell = vim.o.shell, -- change the default shell
+}
+
+vim.cmd([[
+  autocmd TermOpen term://* setlocal laststatus=0
+  autocmd TermLeave term://* setlocal laststatus=2
+]])
+
+require('bookmarks').setup {
+  -- sign_priority = 8,  --set bookmark sign priority to cover other sign
+  save_file = vim.fn.expand "$HOME/.bookmarks", -- bookmarks save file path
+  keywords =  {
+    ["@t"] = "☑️ ", -- mark annotation startswith @t ,signs this icon as `Todo`
+    ["@w"] = "⚠️ ", -- mark annotation startswith @w ,signs this icon as `Warn`
+    ["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+    ["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
+  },
+  on_attach = function(bufnr)
+    local bm = require "bookmarks"
+    local map = vim.keymap.set
+    map("n","mm",bm.bookmark_toggle) -- add or remove bookmark at current line
+    map("n","mi",bm.bookmark_ann) -- add or edit mark annotation at current line
+    map("n","mc",bm.bookmark_clean) -- clean all marks in local buffer
+    map("n","mn",bm.bookmark_next) -- jump to next mark in local buffer
+    map("n","mp",bm.bookmark_prev) -- jump to previous mark in local buffer
+    map("n","ml",bm.bookmark_list) -- show marked file list in quickfix window
+  end
+}
+
+require('log-highlight').setup {
+	-- The following options support either a string or a table of strings.
+
+	-- The file extensions.
+	extension = 'log','txt',
+
+	-- The file names or the full file paths.
+	filename = {
+	},
+
+	-- The glob patterns, e.g. `.*%.lg`, `/var/log/.*`.
+	-- Note: `%.` is to match a literal dot (`.`) in a pattern in Lua, but most
+	-- of the time `.` and `%.` here make no observable difference.
+	pattern = {
+	},
+}
